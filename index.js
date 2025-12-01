c8d760734cbfa2758dd837fc37dd777e4e858879
Emily Chooi <emilychooi.k@northeastern.edu>
2025-12-01 05:38:20 -0500

    node server connect

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import session from "express-session";
 import express from 'express';
+import mongoose from "mongoose";
 import Hello from "./Hello.js"
 import Lab5 from "./Lab5/index.js";
 import PathParameters from './Lab5/PathParameters.js';
@@ -11,6 +12,8 @@ import CourseRoutes from "./Kambaz/Courses/routes.js";
 import cors from "cors";
 import AssignmentRoutes from "./Kambaz/Assignments/routes.js";
 import ModulesRoutes from "./Kambaz/Modules/routes.js";
+const CONNECTION_STRING = process.env.DATABASE_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kambaz"
+mongoose.connect(CONNECTION_STRING);
 const app = express()
 
 app.use(cors({credentials: true,
